62c21e1ffb129d529a1d5b8872e52b099867c19b
Andrey.Skripay <lordktan@gmail.com>
2023-09-11 15:17:40 +0500

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         var dog = 8.0;
+        dog = dog + 4;
         System.out.println(dog);
         var cat = 3.6;
+        cat = 3.6 + 4;
         System.out.println(cat);
         var paper = 763789;
+        paper = 763789 + 4 ;
         System.out.println(paper);
     }
 }
